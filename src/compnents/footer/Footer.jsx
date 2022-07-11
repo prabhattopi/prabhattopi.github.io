27b2d3f5cbf6bf53852831ce1dd978a62741b7c8
prabhattopi <topi9864@gmail.com>
2022-07-11 21:19:49 +0530

    The Final Submission

--- src/compnents/footer/Footer.jsx
+++ src/compnents/footer/Footer.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import "./foot.css"
+import {FaFacebookF} from "react-icons/fa"
+import {FiInstagram} from "react-icons/fi"
+import {IoLogoTwitter} from "react-icons/io"
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer'></a>
+      <a href="#" className='footer__logo'>
+        PRABHAT
+
+      </a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        {/* <li><a href="#testimonials">Testimonials</a></li> */}
+        <li><a href="#contact">Contact</a></li>
+
+         
+      </ul>
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/profile.php?id=100080030157284"><FaFacebookF/></a>
+        <a href="https://instagram.com"><FiInstagram/></a>
+        <a href="https://twitter.com/NumericalWallah"><IoLogoTwitter/></a>
+        {/* <a href="https://facebook.com"></a> */}
+
+
+      </div>
+
+
+      <div className="footer__copyright">
+        <small>&copy; PRABHAT Portfolio. All rights reserved</small>
+      </div>
     </footer>
   )
 }
